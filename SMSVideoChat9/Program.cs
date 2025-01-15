360f0b2d68e389e0b101289ad7f8b651c6cbf5b6
Paul Chavaux <paul.chavaux@outlook.com>
2025-01-15 15:50:38 -0600

    ChatHub

--- SMSVideoChat9/Program.cs
+++ SMSVideoChat9/Program.cs
@@ -1,14 +1,21 @@
 using SMSVideoChat9.Client.Pages;
 using SMSVideoChat9.Components;
-
+using Microsoft.AspNetCore.ResponseCompression;
+using SMSVideoChat9.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSignalR();
 
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
 var app = builder.Build();
-
+app.UseResponseCompression();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -30,5 +37,5 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(SMSVideoChat9.Client._Imports).Assembly);
-
+app.MapHub<ChatHub>("/chathub");
 app.Run();
